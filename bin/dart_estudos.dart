82828c0a7ee5109ae2f26a6942662c62f326fff8
carlospdss <carlosantonio.pereiradasilva@gmail.com>
2026-03-12 21:23:00 -0300

    Aprendendo variáveis e padrões de nomenclatura

--- bin/dart_estudos.dart
+++ bin/dart_estudos.dart
@@ -1,5 +1,9 @@
 import 'package:dart_estudos/dart_estudos.dart' as dart_estudos;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_estudos.calculate()}!');
+  String nomeUsuario = "Carlos";
+  int idadeUsuario = 28;
+
+  print("Nome: $nomeUsuario");
+  print("Idade: $idadeUsuario");
 }
