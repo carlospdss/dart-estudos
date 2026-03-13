3322f5232b57eca43d03962a83994f85c83c4151
carlospdss <carlosantonio.pereiradasilva@gmail.com>
2026-03-12 21:07:45 -0300

    Criando estrutura inicial do projeto Dart

---
+++ bin/dart_estudos.dart
@@ -0,0 +1,5 @@
+import 'package:dart_estudos/dart_estudos.dart' as dart_estudos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_estudos.calculate()}!');
+}
---
+++ test/dart_estudos_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_estudos/dart_estudos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
