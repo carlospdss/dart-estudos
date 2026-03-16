ae2606e1b5a7013257021a2e89277b9ef0485d36
carlospdss <carlosantonio.pereiradasilva@gmail.com>
2026-03-16 15:54:40 -0300

    Aprendendo Laços de Repetição

--- bin/dart_estudos.dart
+++ bin/dart_estudos.dart
@@ -1,11 +1,5 @@
 void main() {
-  String status = "modificado";
-
-  if (status == "modificado"){
-    print("Execute o git add .");
-  } else if (status == "staged"){
-    print("Execute o git commit");
-  } else {
-    print ("Tudo ataulizado");
+  for(int i = 1; i <= 3; i++){
+    print("Verificando arquivo $i");
   }
 }
