f0c06dc0213e3a2f889eee01bbb4bb78030b64b1
carlospdss <carlosantonio.pereiradasilva@gmail.com>
2026-03-16 16:11:19 -0300

    Aprendendo Listas

--- bin/dart_estudos.dart
+++ bin/dart_estudos.dart
@@ -1,6 +1,6 @@
 void main() {
   List<String> alteracoes = ["adicionado", "modificado", "removido"];
-  for (int i = 0; i <= alteracoes.length; i++) {
+  for (int i = 0; i < alteracoes.length; i++) {
     print(alteracoes[i]);
   }
 }
