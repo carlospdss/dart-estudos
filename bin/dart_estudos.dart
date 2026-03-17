bcc3be06a89e906dfa85168b03936f1d4ba301df
carlospdss <carlosantonio.pereiradasilva@gmail.com>
2026-03-17 19:46:38 -0300

    Aprendendo sobre classes e objetos

--- bin/dart_estudos.dart
+++ bin/dart_estudos.dart
@@ -1,9 +1,25 @@
-String criarSaudacao (String nome) {
-    return "Bem-vindo, $nome!";
+class Usuario {
+  String nome;
+  int seguidores;
+
+  Usuario(this.nome, this.seguidores);
+
+  String postar() {
+    return "Postagem realizada!";
   }
 
-void main() {
-  String mensagemFinal = criarSaudacao("Carlos");
-
-  print(mensagemFinal);
+  String quantidadeSeguidores() {
+    return "O usuário $nome tem $seguidores seguidores.";
+  }
 }
+
+void main() {
+  Usuario usuario1 = Usuario("Carlos", 90);
+  Usuario usuario2 = Usuario("Ana", 1500);
+
+  print(usuario1.postar());
+  print(usuario1.quantidadeSeguidores());
+  print(usuario2.postar());
+  print(usuario2.quantidadeSeguidores());
+}
+
